bc76e20369ad3aecd5641dbce178b8c45f46985f
Kailash Parihar <kailashparihar1999@gmail.com>
2025-05-10 23:36:48 +0530

    setup authentication routes

---
+++ src/routes/auth.route.js
@@ -0,0 +1,22 @@
+// backend/src/routes/auth.route.js
+
+import express from "express";
+import {
+  checkAuth,
+  login,
+  logout,
+  signup,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/protectRoute.middleware.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.put("update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
+
+export default router;
